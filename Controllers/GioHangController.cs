2040026a472aafb0e889f9de219145d9392c719e
TeeNoiz04 <duoc14525@gmail.com>
2024-11-08 16:31:24 +0700

    ThucHanhLab2

---
+++ Controllers/GioHangController.cs
@@ -0,0 +1,204 @@
+﻿using MvcBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using MvcBookStore.Models;
+using System.Net;
+namespace MvcBookStore.Controllers
+{
+    public class GioHangController : Controller
+    {
+        // GET: GioHang
+        public ActionResult Index()
+        {
+            return View();
+        }
+        QLBANSACHEntities data = new QLBANSACHEntities();
+
+        // Lấy giỏ hàng
+        public List<GioHang> Laygiohang()
+        {
+            List<GioHang> lstGiohang = Session["Giohang"] as List<GioHang>;
+            if (lstGiohang == null)
+            {
+                // Nếu giỏ hàng chưa tồn tại thì khởi tạo listGiohang
+                lstGiohang = new List<GioHang>();
+                Session["Giohang"] = lstGiohang;
+            }
+            return lstGiohang;
+        }
+        // Thêm hàng vào giỏ
+        public ActionResult ThemGiohang(int iMasach, string strURL)
+        {
+            Console.WriteLine(iMasach);
+            // Lấy ra Session giỏ hàng
+            List<GioHang> lstGiohang = Laygiohang();
+
+            // Kiểm tra sách này đã tồn tại trong Session["Giohang"] chưa?
+            GioHang sanpham = lstGiohang.Find(n => n.iMasach == iMasach);
+            if (sanpham == null)
+            {
+                // Nếu chưa tồn tại, tạo mới sản phẩm
+                sanpham = new GioHang(iMasach);
+                lstGiohang.Add(sanpham); // Thêm sản phẩm mới vào giỏ hàng
+                return Redirect(strURL); // Chuyển hướng về trang hiện tại
+            }
+            else
+            {
+                // Nếu đã tồn tại, tăng số lượng sản phẩm
+                sanpham.iSoluong++;
+                return Redirect(strURL); // Chuyển hướng về trang hiện tại
+            }
+
+        }
+        // Tính tổng số lượng
+        private int TongSoLuong()
+        {
+            int iTongSoLuong = 0;
+            List<GioHang> lstGiohang = Session["Giohang"] as List<GioHang>;
+            if (lstGiohang != null)
+            {
+                // Tổng số lượng bằng tổng số lượng của từng sản phẩm
+                iTongSoLuong = lstGiohang.Sum(n => n.iSoluong);
+            }
+            return iTongSoLuong;
+        }
+        // Tính tổng tiền
+        private double TongTien()
+        {
+            double iTongTien = 0;
+            List<GioHang> lstGiohang = Session["Giohang"] as List<GioHang>;
+            if (lstGiohang != null)
+            {
+                // Tổng tiền bằng tổng thành tiền của từng sản phẩm
+                iTongTien = lstGiohang.Sum(n => n.dThanhtien);
+            }
+            return iTongTien;
+        }
+        // Xây dựng trang Giỏ hàng
+        public ActionResult GioHang()
+        {
+            List<GioHang> lstGiohang = Laygiohang();
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "BookStore"); // Chuyển hướng về trang Index nếu giỏ hàng rỗng
+            }
+            ViewBag.Tongsoluong = TongSoLuong(); // Hiển thị tổng số lượng sản phẩm
+            ViewBag.TongTien = TongTien(); // Hiển thị tổng tiền
+            return View(lstGiohang); // Trả về danh sách giỏ hàng
+        }
+
+        public ActionResult GiohangPartial()
+        {
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+            return PartialView();
+        }
+        // Xóa Giỏ hàng
+        public ActionResult XoaGiohang(int iMaSP)
+        {
+            // Lấy giỏ hàng từ Session
+            List<GioHang> lstGiohang = Laygiohang();
+
+            // Kiểm tra sách đã có trong Session["Giohang"]
+            GioHang sanpham = lstGiohang.SingleOrDefault(n => n.iMasach == iMaSP);
+
+            // Nếu tồn tại thì cho sửa số lượng
+            if (sanpham != null)
+            {
+                lstGiohang.RemoveAll(n => n.iMasach == iMaSP);
+                return RedirectToAction("GioHang");
+            }
+
+            if (lstGiohang.Count == 0)
+            {
+                return RedirectToAction("Index", "BookStore");
+            }
+
+            return RedirectToAction("GioHang");
+        }
+        // Cập nhật Giỏ hàng
+        public ActionResult CapnhatGiohang(int iMaSP, FormCollection f)
+        {
+            // Lấy giỏ hàng từ Session
+            List<GioHang> lstGiohang = Laygiohang();
+
+            // Kiểm tra sách đã có trong Session["Giohang"]
+            GioHang sanpham = lstGiohang.SingleOrDefault(n => n.iMasach == iMaSP);
+
+            // Nếu tồn tại thì cho sửa số lượng
+            if (sanpham != null)
+            {
+                sanpham.iSoluong = int.Parse(f["txtSoluong"].ToString());
+            }
+
+            return RedirectToAction("GioHang");
+        }
+        public ActionResult XoaTatcaGiohang()
+        {
+            // Lấy giỏ hàng từ Session
+            List<GioHang> lstGiohang = Laygiohang();
+            lstGiohang.Clear();
+            return RedirectToAction("Index", "BookStore");
+        }
+        // Hiển thị View DatHang để cập nhật các thông tin cho Đơn hàng
+        [HttpGet]
+        public ActionResult DatHang()
+        {
+            // Kiểm tra đăng nhập
+            if (Session["Taikhoan"] == null || Session["Taikhoan"].ToString() == "")
+            {
+                return RedirectToAction("Dangnhap", "Nguoidung");
+            }
+
+            if (Session["Giohang"] == null)
+            {
+                return RedirectToAction("Index", "BookStore");
+            }
+
+            // Lấy giỏ hàng từ Session
+            List<GioHang> lstGiohang = Laygiohang();
+            ViewBag.Tongsoluong = TongSoLuong();
+            ViewBag.Tongtien = TongTien();
+
+            return View(lstGiohang);
+        }
+        public ActionResult DatHang(FormCollection collection)
+        {
+            // Thêm Đơn hàng
+            DONDATHANG ddh = new DONDATHANG();
+            KHACHHANG kh = (KHACHHANG)Session["Taikhoan"];
+            List<GioHang> gh = Laygiohang();
+            ddh.MaKH = kh.MaKH;
+            ddh.NgayDH = DateTime.Now;
+            var ngaygiao = String.Format("{0:MM/dd/yyyy}", collection["Ngaygiao"]);
+            ddh.Ngaygiao = DateTime.Parse(ngaygiao);
+            ddh.Tinhtranggiaohang = false;
+            ddh.Dathanhtoan = false;
+            data.DONDATHANGs.Add(ddh);
+            data.SaveChanges();
+
+            // Thêm chi tiết đơn hàng
+            foreach (var item in gh)
+            {
+                CHITIETDONTHANG ctdh = new CHITIETDONTHANG();
+                ctdh.SoDH = ddh.SoDH;
+                ctdh.Masach = item.iMasach;
+                ctdh.Soluong = item.iSoluong;
+                ctdh.Dongia = (decimal)item.dDongia;
+                data.CHITIETDONTHANGs.Add(ctdh);
+            }
+
+            data.SaveChanges();
+            Session["Giohang"] = null;
+            return RedirectToAction("Xacnhandonhang", "GioHang");
+        }
+        public ActionResult Xacnhandonhang()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Controllers/NguoiDungController.cs
@@ -0,0 +1,133 @@
+﻿using MvcBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBookStore.Controllers
+{
+
+    public class NguoiDungController : Controller
+    {
+        private readonly QLBANSACHEntities db = new QLBANSACHEntities();
+        // GET: NguoiDung
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangky(FormCollection collection, KHACHHANG kh)
+        {
+            // Gán các giá trị người dùng nhập liệu cho các biến
+            var hoten = collection["HotenKH"];
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+            var matkhaunhaplai = collection["Matkhaunhaplai"];
+            var diachi = collection["Diachi"];
+            var email = collection["Email"];
+            var dienthoai = collection["Dienthoai"];
+            var ngaysinh = String.Format("{0:MM/dd/yyyy}", collection["Ngaysinh"]);
+
+            if (String.IsNullOrEmpty(hoten))
+            {
+                ViewData["Loi1"] = "Họ tên khách hàng không được để trống";
+            }
+            else if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi2"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi3"] = "Phải nhập mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(matkhaunhaplai))
+            {
+                ViewData["Loi4"] = "Phải nhập lại mật khẩu";
+            }
+            else if (String.IsNullOrEmpty(email))
+            {
+                ViewData["Loi5"] = "Email không được bỏ trống";
+            }
+            else if (String.IsNullOrEmpty(dienthoai))
+            {
+                ViewData["Loi6"] = "Phải nhập điện thoại";
+            } else if (String.IsNullOrEmpty(diachi))
+            {
+                ViewData["Loi7"] = "Phải nhập địa chỉ";
+            }
+            else
+            {
+                // Gán giá trị cho đối tượng được tạo mới (kh)
+                kh.HoTen = hoten;
+                kh.Taikhoan = tendn;
+                kh.Matkhau = matkhau;
+                kh.Email = email;
+                kh.DiachiKH = diachi;
+                kh.DienthoaiKH = dienthoai;
+                kh.Ngaysinh = DateTime.Parse(ngaysinh);
+
+                //db.KHACHHANGs.Add(kh);
+                //db.SaveChanges();
+                //return RedirectToAction("Dangnhap");
+                try
+                {
+                    db.KHACHHANGs.Add(kh);
+                    db.SaveChanges();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message); // Hiển thị lỗi ra console
+                    ViewBag.Error = "Có lỗi xảy ra: " + ex.Message; // Hoặc hiển thị trên giao diện
+                    return RedirectToAction("Dangnhap");
+                }
+            }
+
+            return this.DangKy();
+        }
+
+       
+        public ActionResult DangNhap(FormCollection collection)
+        {
+            // Gán các giá trị người dùng nhập liệu cho các biến
+            var tendn = collection["TenDN"];
+            var matkhau = collection["Matkhau"];
+
+            if (String.IsNullOrEmpty(tendn))
+            {
+                ViewData["Loi1"] = "Phải nhập tên đăng nhập";
+            }
+            else if (String.IsNullOrEmpty(matkhau))
+            {
+                ViewData["Loi2"] = "Phải nhập mật khẩu";
+            }
+            else
+            {
+                // Gán giá trị cho đối tượng được tạo mới (kh)
+                KHACHHANG kh = db.KHACHHANGs.SingleOrDefault(
+                    n => n.Taikhoan == tendn && n.Matkhau == matkhau);
+
+                if (kh != null)
+                {
+                    ViewBag.Thongbao = "Chúc mừng đăng nhập thành công";
+                    Session["Taikhoan"] = kh;
+                    return RedirectToAction("Index", "Bookstore");
+                }
+                else
+                {
+                    ViewBag.Thongbao = "Tên đăng nhập hoặc mật khẩu không đúng";
+                }
+            }
+            return View();
+        }
+       
+
+
+
+    }
+}
